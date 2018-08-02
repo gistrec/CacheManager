1eff8c48461c3818b3437322a4768bb773977906
gistrec <gistrec@mail.ru>
2018-08-02 10:29:57 +0300

    add FileSystemHandler

---
+++ src/Handler/FileSystemHandler.php
@@ -0,0 +1,72 @@
+<?php
+
+require './IHandler.php';
+
+class FileSystemHandler interface IHandler {
+
+	private $path;
+
+	public function __construct($path) {
+		// Добовляем слеш в конце пути, если его нет
+		if ($path[strlen($path) - 1] != '/') {
+			$path .= '/';
+		}
+		$this->path = $path;
+	}
+
+	public function set($key, $data, $ttl = -1) {
+		$cacheFile = $this->path . md5($key) . '.cache';
+		$timeFile  = $this->path . md5($key) . '.time';
+
+		if ($ttl = -1) {
+			$ttl = PHP_MAX_INT;
+		}else {
+			$ttl = time() + $ttl;
+		}
+		$data = $this->serialize($data);
+		file_put_contents($this->path . md5($key) . '.cache', $data);
+		file_put_contents($this->path . md5($key) . '.time',  $ttl)
+	}
+
+	public function get($key) {
+		$cacheFile = $this->path . md5($key) . '.cache';
+		$timeFile  = $this->path . md5($key) . '.time';
+
+		if (file_exists($cacheFile) && file_exists($timeFile)) {
+			$ttl = file_get_contents($timeFile);
+			// Проверяем акутальность данных
+			if ($ttl < time()) {
+				$this->del($key);
+				return NULL;
+			}else {
+
+				$data = file_get_contents($cacheFile);
+				$data = $this->unserialize($data);
+				return $data;
+			}
+		}else {
+			return NULL;
+		}
+	}
+
+	public function del($key) {
+		$cacheFile = $this->path . md5($key) . '.cache';
+		$timeFile  = $this->path . md5($key) . '.time';
+
+		if (file_exists($cacheFile)) {
+			unlink($cacheFile);
+			unlink($timeFile);
+			return true;
+		}else {
+			return false;
+		}
+	}
+
+	private function serialize($data) {
+        return serialize($data);
+    }
+
+    private function unserialize($data) {
+        return unserialize($data);
+    }
+}
---
+++ src/Handler/IHandler.php
@@ -0,0 +1,27 @@
+<?
+
+interface IHandler {
+
+	/**
+     * Добавление данных
+     * @param string $key
+     * @param mixed $data
+     * @param int ttl Время жизни данных
+     * @return mixed Возвращает NULL при неудаче, или хранимые данные
+     */
+	public function set($key, $data, $ttl = -1);
+
+	/**
+     * Удаление данных
+     * @param string $key
+     * @return mixed Возвращает NULL при неудаче, или хранимые данные
+     */
+	public function get($key);
+
+	/**
+     * Удаление данных
+     * @param string $key
+     * @return bool Возвращает TRUE при успехе, иначе FALSE
+     */
+	public function del($key);
+}
